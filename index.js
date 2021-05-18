7a3bc8892f613126ffd58109a69d5433fbd7b74b
Alex Calvert <alexanderajcalvert@gmail.com>
2021-05-17 19:02:50 -0700

    commit tree 1566d4454a81a8e7e8561ff3f7cbdad21d7aef44

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street)*264;
+}
+
+function distanceTravelledInFeet(start, end) {
+  return Math.abs(start - end)*264;
+}
+
+function calculatesFarePrice(start, end) {
+    let feet = distanceTravelledInFeet(start, end); 
+    if (feet < 400) {
+        return 0;
+    }  else if (feet < 2000) {
+        return (feet - 400)*0.02;
+    }  else if (feet < 2500) {
+        return 25;
+    }  else if (feet >2500) {
+        return 'cannot travel that far';
+    }
+}
